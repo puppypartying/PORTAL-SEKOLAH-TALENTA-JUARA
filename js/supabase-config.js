dee7f5336bfc51c4d55da867e85b7a36ca3535c8
puppypartying <qeis24001@mail.unpad.ac.id>
2026-06-26 18:46:36 +0700

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,11 +1,6 @@
-/* ════════════════════════════════════════════════════════════════
-   KONFIGURASI SUPABASE
-   Ganti dua nilai di bawah ini dengan punya Anda.
-   Lihat panduan lengkap di file PANDUAN-SUPABASE.md
-   ════════════════════════════════════════════════════════════════ */
 
-const SUPABASE_URL = 'https://armgeupbybrroalzhaqz.supabase.co';   // <- ganti dengan Project URL Anda
-const SUPABASE_ANON_KEY = 'sb_publishable_QtgU7IoO7ESLB1zneEd-eA_2_6HUGin'; // <- ganti dengan anon public key Anda
 
+const SUPABASE_URL = 'https://ydrtrtqbpnpiqejnhlvo.supabase.co';   
+const SUPABASE_ANON_KEY = 'sb_publishable_2kM7Av7fjcfwU4O-1NspcA_jT5UFAC5'; 
 /* Jangan ubah baris di bawah ini */
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
